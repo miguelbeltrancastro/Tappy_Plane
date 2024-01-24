fb310df7425912fb805cb458c366de45ece21c7b
Miguel <miguel.beltran_castro@mercedes-benz.com>
2024-01-24 11:46:46 +0100

    Game over done

--- scenes/game_over/game_over.gd
+++ scenes/game_over/game_over.gd
@@ -1,11 +1,28 @@
 extends Control
 
+@onready var go_label = $GOLabel
+@onready var space_label = $SpaceLabel
+@onready var timer = $Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	hide()
+	SignalManager.on_plane_died.connect(on_plane_died)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if space_label.visible == true and Input.is_action_just_pressed("fly") == true:
+		GameManager.load_main_scene()
+
+func on_plane_died() -> void:
+	show()
+	timer.start()
+
+func run_sequence() -> void:
+	go_label.hide()
+	space_label.show()
+	
+
+func _on_timer_timeout():
+	run_sequence()
